dfec88ad84bc491383923f3f9aa19447d4cb8bbc
Mike Kinney <mike.kinney@gmail.com>
2022-01-04 10:33:58 -0800

    needed to change the entrypoint

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     python_requires='>=3.6',
     entry_points={
         "console_scripts": [
-            "MeshFlash=MeshFlash.__main__:main",
+            "meshflash=MeshFlash.__main__:main",
         ]
     },
 )
